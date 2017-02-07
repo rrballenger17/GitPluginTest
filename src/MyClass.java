5c2d9a5735365cc4c03e2dbdb5d624167daf6dc3
Ryan <Ryan@10.251.228.219>
2017-02-07 18:07:23 -0500

    added all

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,4 +1,15 @@
 
 public class MyClass {
 	private int myINt = 11;
+	
+	   public static void main(String []args) {
+
+		   System.out.println("Hello, World!");
+		   
+		   System.out.println("Hello, Universe!");
+
+		   System.out.println("Hello, All!");
+	   
+	   }
+	
 }
